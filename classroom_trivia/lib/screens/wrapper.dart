21b2670d69595a044052bde53cf34900f572ebce
Hassan <Hassan>
2021-03-14 00:19:51 -0600

    started android app

---
+++ classroom_trivia/lib/screens/wrapper.dart
@@ -0,0 +1,11 @@
+import 'package:classroom_trivia/screens/authenticate/authenticate.dart';
+import 'package:classroom_trivia/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //return home or auth
+    return Authenticate();
+  }
+}
